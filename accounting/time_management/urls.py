8746bb4790e3c5a68b0a6154803c1b483086614d
Bekbolino <133040419+Bekbolino@users.noreply.github.com>
2024-05-07 01:22:40 +0600

    Update urls.py

--- accounting/time_management/urls.py
+++ accounting/time_management/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("gorilla", views.gorilla, name='gorilla'),
+    path("", views.login, name='login'),
+    path("login", views.login, name='login'),
+    path("register", views.register, name='register'),
 ]
