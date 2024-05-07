796a4607caece59ff8108d0452b61554e488e18e
Bekbolino <you@example.combekbolkadyrbekov93@gmail.com>
2024-05-08 00:44:03 +0600

    commit console

---
+++ time_management/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .models import *
+# Create your views here.
+
+def index(request):
+    return render(request, 'time_management/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'time_management/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        name = request['name']
+        password = request['password']
+        password_check = request['password_check']
+        if password == password_check:
+            user = User.objects.create_user(name, password)
+            user.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, 'time_management/register', {"problem": "Пароли не совпадают. Повтортите попытку.."})
+        
+    # else:
+    print(request)
+    return render(request, 'time_management/register.html')
